76ce27f9bc56b21cd8c196bbb3fe27b530361916
sean <42329771+seannlyj@users.noreply.github.com>
2026-04-10 10:24:03 +0800

    Update Neo_Sean_Player.java

--- Assignment 2/Neo_Sean_Player.java
+++ Assignment 2/Neo_Sean_Player.java
@@ -1,17 +1,3 @@
-abstract class Player {
-    // This procedure takes in the number of rounds elapsed so far (n), and
-    // the previous plays in the match, and returns the appropriate action.
-    int selectAction(int n, int[] myHistory, int[] oppHistory1, int[] oppHistory2) {
-        throw new RuntimeException("You need to override the selectAction method.");
-    }
-
-    // Used to extract the name of this player class.
-    final String name() {
-        String result = getClass().getName();
-        return result.substring(result.indexOf('$') + 1);
-    }
-}
-
 class Neo_Sean_Player extends Player {
     int selectAction(int n, int[] myHistory, int[] oppHistory1, int[] oppHistory2) {
 
